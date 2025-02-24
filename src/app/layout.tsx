3ebd901c5f41ee4af72b8d317516dc2730352387
Ben Segev <benasegev@gmail.com>
2025-02-24 16:22:00 +0200

    fix: update favicon configuration in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,15 @@
 import { Inter, Montserrat } from 'next/font/google'
+import { Metadata } from 'next'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 const montserrat = Montserrat({ subsets: ['latin'], variable: '--font-montserrat' })
+
+export const metadata: Metadata = {
+  icons: {
+    icon: '/icons/favicon.ico',
+  },
+}
 
 export default function RootLayout({
   children,
